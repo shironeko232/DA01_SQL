7199080942e5d30049f729ae318b2429a0bdc5d1
shironeko232 <153172183+shironeko232@users.noreply.github.com>
2024-03-11 01:16:20 +0700

    Update PRACTICE7.sql

--- PRACTICE7.sql
+++ PRACTICE7.sql
@@ -14,46 +14,7 @@ FROM cte
 WITH cte AS (SELECT card_name,
 issued_amount,
 MAKE_DATE(issue_year, issue_month, 1) AS issue_date,
-MIN(MAKE_DATE(issue_year, issue_month, 1)) OVER(PARTITION BY card_name) AS launch_date
-FROM monthly_cards_issued)
-SELECT card_name,
-issued_amount
-FROM cte 
-WHERE issue_date = launch_date
-ORDER BY issued_amount DESC 
-
-  -- EX 3
-WITH cte AS ( SELECT 
-user_id, 
-spend, 
-transaction_date, 
-ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY transaction_date) AS row_num 
-FROM transactions )
-SELECT user_id, spend, transaction_date
-FROM cte 
-WHERE row_num = 3
-
-  -- EX 4
-WITH cte AS (SELECT user_id, transaction_date, product_id, 
-RANK() OVER (PARTITION BY user_id ORDER BY transaction_date DESC ) AS ranking 
-FROM user_transactions)
-SELECT transaction_date, user_id, 
-COUNT(product_id ) AS purchase_count
-FROM cte 
-WHERE ranking  = 1 
-GROUP BY transaction_date, user_id
-
-  -- EX 5
-SELECT    
-  user_id,    
-  tweet_date,   
-  ROUND(AVG(tweet_count) OVER (PARTITION BY user_id ORDER BY tweet_date     
-  ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
-  ,2) AS rolling_avg_3d
-FROM tweets
-
-  -- EX 6
-  em đang mắc ạ
+MIN(MAKE_DATE(issue_year, issue_month, 1)) OVERạ
  
   -- EX 7
 WITH bang_xep_hang AS (
